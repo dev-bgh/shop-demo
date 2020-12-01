b7a23a144d2dd5f67efc3f8c681d549edb5a2513
ghometry <ghometry@gmail.com>
2020-12-01 22:10:07 +0900

    기본세팅

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './Main.css';
+
+function App() {
+  return (
+    <body>
+      <div className="App">
+        <div className="App-header">
+          <div className="rightMenu">
+            <div className="mainTitle">기하 샵</div>
+          </div>
+        </div>
+    </div>
+  </body>
+  );
+}
+
+export default App;
